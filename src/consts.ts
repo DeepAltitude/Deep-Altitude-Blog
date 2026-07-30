1c41de54c472086c7bd50ed1792d99038573766b
DeepAltitude <petrauskas.kristupas@gmail.com>
2026-07-30 16:49:33 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Deep Altitude";
+
+export const SITE_DESCRIPTION =
+  "Essays and field notes on attention, learning, risk, and deliberate living.";
